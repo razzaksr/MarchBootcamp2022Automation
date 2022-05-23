e614d044342063de63cf97b23d999ec855db18a6
razzaksr <razzaksr@gmail.com>
2022-05-23 13:44:02 +0530

    basic, in memory authentication

---
+++ src/main/java/backend/poc/first/POC1BackEnd/Config.java
@@ -0,0 +1,39 @@
+package backend.poc.first.POC1BackEnd;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class Config extends WebSecurityConfigurerAdapter 
+{	
+	@Bean
+	public PasswordEncoder getEncode()
+	{
+		return new BCryptPasswordEncoder();
+	}
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		auth.inMemoryAuthentication().withUser("razak").
+		password(getEncode().encode("mohamed")).roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("annamalai").
+		password(getEncode().encode("sam")).roles("ADMIN");
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http.authorizeRequests().anyRequest().authenticated();
+		http.httpBasic();
+		http.formLogin();
+		http.csrf().disable();// post,put,delete
+		//http.authorizeRequests().anyRequest().permitAll();
+	}
+	
+}
--- src/main/java/backend/poc/first/POC1BackEnd/Poc1BackEndApplication.java
+++ src/main/java/backend/poc/first/POC1BackEnd/Poc1BackEndApplication.java
@@ -1,13 +1,31 @@
 package backend.poc.first.POC1BackEnd;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Poc1BackEndApplication {
+	
+	@Autowired
+	ResourceService service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Poc1BackEndApplication.class, args);
 	}
 
+	/*
+	@PostConstruct
+	public void hai()
+	{
+		Resource res1=new Resource(2011, "Manoj Kumar M", new String[] {"java","bs"}, 20000, "Salem");
+		Resource res2=new Resource(1982, "Madheshwaran M", new String[] {"django","flask"}, 19000, "Trichy");
+		Resource res3=new Resource(912, "Titus C", new String[] {"angular","react"}, 24000, "Chennai");
+		Resource res4=new Resource(1201, "Victor T", new String[] {"java","spring"}, 20000, "Madurai");
+		
+		service.create(res1);service.create(res2);service.create(res3);service.create(res4);
+	}
+	*/
 }
