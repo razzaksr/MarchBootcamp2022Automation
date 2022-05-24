fae0d79f9cfbd0479db1a4fa010260e2dbaf4a88
razzaksr <razzaksr@gmail.com>
2022-05-24 13:47:01 +0530

    Bootcamp over

--- src/main/java/backend/poc/first/POC1BackEnd/Poc1BackEndApplication.java
+++ src/main/java/backend/poc/first/POC1BackEnd/Poc1BackEndApplication.java
@@ -23,7 +23,6 @@ public class Poc1BackEndApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Poc1BackEndApplication.class, args);
 	}
-
 //	@PostConstruct
 //	public void hai()
 //	{
